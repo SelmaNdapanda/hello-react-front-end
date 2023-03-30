5917fc088bd1a75c6bcffa121964d6ccdab44779
Selma <selmandapanda@yahoo.com>
2023-03-30 23:59:55 +0200

    Make required changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Greeting from './components/greeting';
-// import { fetchGreeting } from './redux/fetchGreeting';
 
 function App() {
   return (
