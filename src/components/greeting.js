42a730c9ebd5f04ad8e15ba8a30d3390e81c0098
Selma <selmandapanda@yahoo.com>
2023-03-30 21:59:42 +0200

    Create a greeting component

---
+++ src/components/greeting.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchGreeting from '../redux/fetchGreeting';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+  const message = useSelector((state) => state.greet);
+  return <h1>Message: {message.data.text}</h1>;
+}
+
+export default Greeting;
